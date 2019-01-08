59a3777ea17f3321fd27e0b62de057088c8634c3
LarsTufvesson <lars.tufvesson@sellegi.se>
2019-01-08 16:00:05 +0100

    Removed troublesome S3 part

--- jenkins-cf-template.py
+++ jenkins-cf-template.py
@@ -78,19 +78,6 @@ ud = Base64(Join('\n', [ "#!/bin/bash",
   "sudo echo '*/10 * * * * {}' | sudo tee /etc/cron.d/ansible-pull > /dev/null".format(AnsiblePullCmd)
 ]))
 
-t.add_resource(IAMPolicy(
-  "Policy",
-  PolicyName="AllowS3",
-  PolicyDocument=Policy(
-    Statement=[
-      Statement(
-        Effect=Allow,
-        Action=[Action("s3", "*")],
-        Resource=["*"])
-    ]
-  ),
-))
-
 t.add_resource(Role(
   "Role",
   AssumeRolePolicyDocument=Policy(
